5efc7e623d0a05afadb48a236fca075c7e1908e7
howzy <ihowzy@gmail.com>
2017-09-13 23:17:18 +0800

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
     FormsModule,
     BrowserAnimationsModule,
     NgZorroAntdModule.forRoot(),
+    LoginModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/login/login-routing.modult.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      {
+        path: 'login',
+        component: LoginComponent
+      }
+    ])
+  ],
+  exports: [RouterModule]
+})
+export class LoginRoutingModule {
+  
+}
