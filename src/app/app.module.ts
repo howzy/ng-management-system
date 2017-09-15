0f64b7f85e6e9cd9e92a082ce504090e4b0ebb0d
once <zhonce829@gmail.com>
2017-09-15 18:03:10 +0800

    完成用户中心

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 
 import { AppComponent } from './app.component';
@@ -23,6 +23,7 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     NgZorroAntdModule.forRoot(),
     LoginModule,
--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,18 +1,86 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { fadeIn } from '../animations/fade-in';
+import { User } from '../login/model/user';
+import { LoginService } from '../login/services/login.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
-  styleUrls: ['./user-profile.component.css'],
+  styleUrls: ['./user-profile.component.css' ],
   animations: [fadeIn]
 })
 export class UserProfileComponent implements OnInit {
+  userProfileForm: FormGroup;
+  user: User = <User>{};
+  checkPassword: string;
+  hasGetCaptcha: boolean;
+  second: number = 60;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private location: Location,
+    private loginService: LoginService
+  ) { }
 
   ngOnInit() {
+    this.user = JSON.parse(sessionStorage.getItem('currentUser'));
+    this.buildForm();
+  }
+
+  buildForm() {
+    this.userProfileForm = this.fb.group({
+      userName: [ this.user.userName, [ Validators.required ] ],
+      password: [ this.user.password, [ Validators.required ] ],
+      checkPassword: [ this.checkPassword, [ Validators.required, this.confirmationValidator ] ],
+      phonePrefix: [ '+86' ],
+      phone: [ null, [ Validators.required ] ],
+      captcha: [ null, [ Validators.required ] ]
+    })
+  }
+
+  updateConfirmValidator() {
+    setTimeout(_ => {
+      this.userProfileForm.controls['checkPassword'].updateValueAndValidity();
+    });
+  }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    } else if (control.value !== this.userProfileForm.controls['password'].value) {
+      return { confirm: true, error: true };
+    }
+  };
+
+  getFormControl(name) {
+    return this.userProfileForm.controls[name];
+  }
+
+  getCaptcha(e: MouseEvent) {
+    this.hasGetCaptcha = true;
+    this.second = 60;
+
+    let timer = setInterval(() => {
+      if (this.second <= 0) {
+        clearInterval(timer);
+        this.hasGetCaptcha = false;
+      } else {
+        this.second--;
+      }
+    }, 1000);
+    
+    e.preventDefault();
+  }
+
+  cancel() {
+    this.location.back();
+  }
+
+  submitForm() {
+    this.location.back();
   }
 
 }
