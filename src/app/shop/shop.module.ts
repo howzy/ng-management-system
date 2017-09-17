e258a7dbf679d75bad19ea2b21e228924b67c7d6
howzy <ihowzy@gmail.com>
2017-09-17 23:55:06 +0800

    店铺信息添加面包屑导航

--- src/app/shop/shop.module.ts
+++ src/app/shop/shop.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+
 import { ShopInfoComponent } from './shop-info/shop-info.component';
 import { ShopRoutingModule } from './shop-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    ShopRoutingModule
+    ShopRoutingModule,
+    NgZorroAntdModule
   ],
   declarations: [
     ShopInfoComponent
