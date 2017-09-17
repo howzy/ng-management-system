94c55ce2203cb89646bc3ead2e71facecec3f343
howzy <ihowzy@gmail.com>
2017-09-17 16:34:38 +0800

    动态生成面包屑导航

--- src/app/components/breadcrumb/breadcrumb.component.ts
+++ src/app/components/breadcrumb/breadcrumb.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import { fadeIn } from '../../animations/fade-in';
+import { SideBarService } from '../side-bar/services/side-bar.service';
+import { SideBar } from '../side-bar/model/menu';
 
 @Component({
   selector: 'app-breadcrumb',
   templateUrl: './breadcrumb.component.html',
-  styleUrls: ['./breadcrumb.component.css']
+  styleUrls: ['./breadcrumb.component.css'],
+  animations: [fadeIn]
 })
-export class BreadcrumbComponent implements OnInit {
+export class BreadcrumbComponent implements OnInit, OnDestroy {
+  menu: SideBar = <SideBar>{};
+  subscription: Subscription;
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private sideBarService: SideBarService
   ) { }
 
   ngOnInit() {
+    this.subscription = this.sideBarService.menu.subscribe(menu => {
+      this.menu = menu;
+    })
+  }
+  
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
