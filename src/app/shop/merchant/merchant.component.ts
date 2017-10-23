7199703e79244f9d6cc0edcc2ad8a9dda317e373
once <zhonce829@gmail.com>
2017-10-23 18:59:29 +0800

    完成门店管理列表页

--- src/app/shop/merchant/merchant.component.ts
+++ src/app/shop/merchant/merchant.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from "@angular/router";
+
+import { NzModalService, NzMessageService } from 'ng-zorro-antd';
 
 import { fadeIn } from "../../animations/fade-in";
 import { ShopService } from "../services/shop.service";
 import { Merchant } from "../model/merchant";
+import { FieldBase, Textbox } from "../../shared/components/dynamic-form/form-field";
 
 @Component({
   selector: 'app-merchant',
@@ -17,13 +21,61 @@ export class MerchantComponent implements OnInit {
   pageIndex: number = 1;
   pageSize: number = 10;
   loading: boolean = true;
+  isVisible = false;
+  selectedImg: string;
+
+  showModal = (imgUrl) => {
+    this.selectedImg = imgUrl;
+    this.isVisible = true;
+  }
+
+  handleOk = (e) => {
+    this.isVisible = false;
+  }
+
+  handleCancel = (e) => {
+    this.isVisible = false;
+  }
+
+  // 搜索栏条目
+  serachList: FieldBase<any>[] = [
+    new Textbox({
+      key: 'merchantName',
+      label: '门店名称',
+      placeholder: '请输入门店名称'
+    }),
+    new Textbox({
+      key: 'phone',
+      label: '联系电话',
+      placeholder: '请输入联系电话'
+    }),
+    new Textbox({
+      key: 'recommendMemberNo',
+      label: '推荐人账号',
+      placeholder: '请输入推荐人账号'
+    }),
+    new Textbox({
+      key: 'recommendPhone',
+      label: '推荐人手机号',
+      placeholder: '请输入推荐人手机号'
+    })
+  ];
 
   constructor(
-    private shopService: ShopService
+    private shopService: ShopService,
+    private confirmServ: NzModalService,
+    private messageServ: NzMessageService,
+    private router: Router,
+    private route: ActivatedRoute
   ) { }
 
   ngOnInit() {
     this.getMerchantList();
+  }
+
+  // 搜索
+  getSearchList(value) {
+    console.log(value);
   }
 
   getMerchantList() {
@@ -31,8 +83,33 @@ export class MerchantComponent implements OnInit {
     this.shopService.getMerchantList().subscribe(res => {
       this.loading = false;
       this.merchantList = res;
-      console.log(res);
     });
+  }
+
+  delShopTypeById(typeId) {
+    this.confirmServ.confirm({
+      title: '是否要删除该分类？',
+      onOk: () => {
+        this.messageServ.success('删除成功！');
+      }
+    });
+  }
+
+  batchDel() {
+    if (this.indeterminate || this.allChecked) {
+      this.confirmServ.confirm({
+        title: '是否批量删除分类？',
+        onOk: () => {
+          this.messageServ.success('删除成功！');
+        }
+      });
+    } else {
+      this.messageServ.create('warning', '请至少选择一行！');
+    }
+  }
+
+  gotoEdit(merchantId?: string) {
+    this.router.navigate(['./edit', merchantId ? { id: merchantId } : {}], { relativeTo: this.route });
   }
 
   updateAllChecked() {
--- src/app/shop/model/merchant.ts
+++ src/app/shop/model/merchant.ts
@@ -1,20 +1,38 @@
 export class Merchant {
+  id: string;
+  /** 地址 */
   address_detail: Date;
+  /** 创建日期 */
   create_date: string;
+  /** 昵称 */
   nickname: string;
+  /** 推荐人账号 */
   recommend_member_no: string;
+  /** 推荐人昵称 */
   recommend_nickname: string;
+  /** 推荐人手机号 */
   recommend_phone: string;
+  /** 门店图片 */
   merchant_img: string;
+  /** 门店二维码 */
   merchant_QRCode: string;
+  /** 门店名称 */
   merchant_name: string;
+  /** 门店简介 */
   merchant_desc: string;
+  /** 联系电话 */
   phone: string;
+  /** 门店折扣 */
   discount: number;
+  /** 省编码 */
   provinceCode: string;
+  /** 市编码 */
   cityCode: string;
+  /** 区编码 */
   areaCode: string;
+  /** 经度 */
   coordinate_x: string;
+  /** 维度 */
   coordinate_y: string;
   checked?: boolean;
 }
